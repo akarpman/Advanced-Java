b476cecb4862b09f0add91d57210dc8818d95c3b
akarpman <46983118+akarpman@users.noreply.github.com>
2019-01-28 20:07:09 -0800

    Add files via upload

---
+++ first one/src/ComplexNumbers.java
@@ -0,0 +1,18 @@
+
+public class ComplexNumbers {
+	private double real;
+	private double imaginary;
+	ComplexNumbers(double r, double im){
+		
+	};
+	void add(ComplexNumbers im)
+	{
+		real = r;
+		imaginary = (double)im;
+	}
+	
+	public class ComlpexCalculator{
+		
+	}
+
+}
