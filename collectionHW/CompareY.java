7fd775c0e8d1a840599ae0d90eb7d7092bfb64e5
akarpman <46983118+akarpman@users.noreply.github.com>
2019-02-28 17:04:06 -0800

    Update CompareY.java

--- collectionHW/CompareY.java
+++ collectionHW/CompareY.java
@@ -1,20 +1,35 @@
+
 package collectionHW;
 
 import java.util.Comparator;
 
-public abstract class CompareY implements Comparator<Point>{
+public class CompareY implements Comparator<Point>{
 
 	int x;
 	int y;
+	CompareY()
+	{
+		x = 0;
+		y = 0;
+	}
 	CompareY(int _x, int _y){
 		x= _x;
 		y = _y;
 	}
-	public boolean compareTo(int _x, int _y) {
-		if (y == _y)
-			if(x == _x)
-				return true;
-		return false;
+	@Override
+	public int compare(Point o1, Point o2) {
+		if (o1.y == o2.y) {
+			if(o1.x == o2.x)
+				return 0;
+		}
+		else if (o1.y < o2.y)
+			return -1;
+		else
+			return 1;
+		if (o1.x < o2.x)
+			return -1;
+		else
+			return 1;
 	}
 
 }
